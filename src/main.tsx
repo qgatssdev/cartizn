bd0c1c7ecedbbd70c69a7947bc42dba365c661e4
qgatssdev <chivoomodu@gmail.com>
2023-09-14 23:47:24 +0100

    add font

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import './font/QarkinePersonalUse-ZVvn3.ttf';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
