0cee756bba493999a0a2ebd88d48b5809fe4964e
qgatssdev <chivoomodu@gmail.com>
2023-09-26 23:03:58 +0100

    create event page

---
+++ src/pages/events/index.tsx
@@ -0,0 +1,29 @@
+import './styles.scss';
+
+const Events = () => {
+  return (
+    <div className='events_main__container'>
+      <div className='events_wrapper'>
+        <div className='events_header'>
+          <div className='events_link'>
+            <p>CONCERTS & EVENTS 🔥</p>
+          </div>
+          <div className='events_link'>
+            <p>PORTRAIT SHOOT 📸</p>
+          </div>
+          <div className='events_link'>
+            <p>CREATIVE SHOOT ✨</p>
+          </div>
+          <div className='events_link'>
+            <p>LETS TALK 📨</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Events;
