6d376c26b301ce4d8ad2b604e8fa4aa6fec8a46f
qgatssdev <chivoomodu@gmail.com>
2023-09-26 23:03:15 +0100

    ongoing change

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Home from './pages/home';
 import './App.scss';
+import Events from './pages/events';
 
 function App() {
   return (
     <div className='container'>
-      <Home />
+      {/* <Home /> */}
+      <Events />
     </div>
   );
 }
