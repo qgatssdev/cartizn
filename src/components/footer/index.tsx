eb686c7963bbf488bdd0fb3aba0f2480e0cf357d
qgatssdev <chivoomodu@gmail.com>
2023-09-28 23:51:38 +0100

    new footer

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,14 @@
+import './styles.scss';
+
+const Footer = () => {
+  return (
+    <div className='footer_container'>
+      <p>
+        ©cartizn • All Rights Reserved • Designed by {' '}
+        <span>Israel Olurotimi.</span>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
