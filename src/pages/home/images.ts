086260359735c1f8cbf7dd7ae37036513828501e
qgatssdev <chivoomodu@gmail.com>
2024-01-09 23:00:58 +0100

    chore: create image store

---
+++ src/pages/home/images.ts
@@ -0,0 +1,29 @@
+export const imageContainerOne = [
+  require('../../assets/ayra.png'),
+  require('../../assets/rema.png'),
+  require('../../assets/carti.png'),
+];
+
+export const imageContainerTwo = [
+  require('../../assets/girl1.png'),
+  require('../../assets/girl2.png'),
+  require('../../assets/girl3.png'),
+];
+
+export const imageContainerThree = [
+  require('../../assets/girl2.png'),
+  require('../../assets/girl3.png'),
+  require('../../assets/girl1.png'),
+];
+
+export const imageContainerFour = [
+  require('../../assets/girl3.png'),
+  require('../../assets/ayra.png'),
+  require('../../assets/rema.png'),
+];
+
+export const imageContainerFive = [
+  require('../../assets/girl2.png'),
+  require('../../assets/girl1.png'),
+  require('../../assets/girl3.png'),
+];
