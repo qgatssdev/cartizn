90280a3fabef388fd7437034b7fdb77a6a128877
Uche Omodu <79480742+qgatssdev@users.noreply.github.com>
2024-01-15 23:55:59 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,8 @@ function App() {
   const eventsRef = useRef<HTMLDivElement>(null);
   const [prevScrollY, setPrevScrollY] = useState(0);
 
+  //
+
   useEffect(() => {
     const handleScroll = () => {
       const currentScrollY = window.scrollY;
